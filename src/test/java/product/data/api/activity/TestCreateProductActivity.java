75e1e1505497d414add19ff6b80da4a69086e2e6
Matthew Stibbins <matthewstibbins@gmail.com>
2022-06-27 15:40:53 -0700

    implement TestCreateProductActivity

--- src/test/java/product/data/api/activity/TestCreateProductActivity.java
+++ src/test/java/product/data/api/activity/TestCreateProductActivity.java
@@ -1,6 +1,15 @@
 package product.data.api.activity;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import product.data.api.manager.ProductManager;
 import product.data.api.model.Product;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 public class TestCreateProductActivity {
 
@@ -16,6 +25,24 @@ public class TestCreateProductActivity {
             .productDescription(TEST_PRODUCT_DESCRIPTION)
             .build();
 
-    
+    @Mock
+    private ProductManager productManager;
+
+    private CreateProductActivity createProductActivity;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        createProductActivity = new CreateProductActivity(productManager);
+    }
+
+    @Test
+    public void testCreatePipeline() {
+        when(productManager.createProduct(any(String.class), any(String.class)))
+                .thenReturn(TEST_PRODUCT);
+        Product product = createProductActivity.createProductUrlParams(TEST_PRODUCT_NAME, TEST_PRODUCT_DESCRIPTION)
+                .getBody().getCreatedProduct();
+        assertThat(product).isEqualTo(TEST_PRODUCT);
+    }
 
 }
