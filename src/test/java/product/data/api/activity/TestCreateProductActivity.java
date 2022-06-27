aa77baf3c86f75bf1f4bf3bd1f2c562dcbfa75e1
Matthew Stibbins <matthewstibbins@gmail.com>
2022-06-27 15:35:14 -0700

    add TestCreateProductActivity

--- src/test/java/product/data/api/activity/TestCreateProductActivity.java
+++ src/test/java/product/data/api/activity/TestCreateProductActivity.java
@@ -1,6 +1,20 @@
 package product.data.api.activity;
 
+import product.data.api.model.Product;
+
 public class TestCreateProductActivity {
+
+    private static final Long TEST_PRODUCT_ID = 1L;
+
+    private static final String TEST_PRODUCT_NAME = "testProductName";
+
+    private static final String TEST_PRODUCT_DESCRIPTION = "testProductionDescription";
+
+    private static final Product TEST_PRODUCT = Product.builder()
+            .productId(TEST_PRODUCT_ID)
+            .productName(TEST_PRODUCT_NAME)
+            .productDescription(TEST_PRODUCT_DESCRIPTION)
+            .build();
 
     
 
