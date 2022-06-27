c94f3766377f7245cfcf38e263bccbdf82c5a6ec
Matthew Stibbins <matthewstibbins@gmail.com>
2022-06-27 00:56:03 -0700

    init repo with spring boot project

---
+++ src/main/java/product/data/api/ProductDataApiApplication.java
@@ -0,0 +1,13 @@
+package product.data.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductDataApiApplication.class, args);
+	}
+
+}
