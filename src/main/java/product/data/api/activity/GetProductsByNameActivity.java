df7f824897c8d25830550afe9f8e856c10d3c466
Matthew Stibbins <matthewstibbins@gmail.com>
2022-06-27 04:58:43 -0700

    implement GetProductsByNameActivity

--- src/main/java/product/data/api/activity/GetProductsByNameActivity.java
+++ src/main/java/product/data/api/activity/GetProductsByNameActivity.java
@@ -1,10 +1,17 @@
 package product.data.api.activity;
 
+import java.util.List;
+
 import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import product.data.api.manager.ProductManager;
+import product.data.api.model.Product;
+import product.data.api.response.GetProductsByNameResponse;
 
 @RestController
 public class GetProductsByNameActivity {
@@ -16,6 +23,17 @@ public class GetProductsByNameActivity {
 		this.productManager = Validate.notNull(productManager);
 	}
 	
-	
+	public ResponseEntity<GetProductsByNameResponse> getProductsByNameUrlParams(
+			@RequestParam String productName
+	) {
+		Validate.notNull(productName, "productName cannot be null");
+		List<Product> productsList = productManager.getProductsByName(productName);
+		GetProductsByNameResponse getProductsByNameResponse = GetProductsByNameResponse.builder()
+				.productsList(productsList)
+				.build();
+		ResponseEntity<GetProductsByNameResponse> responseEntityGetProductsByNameResponse = 
+				new ResponseEntity<>(getProductsByNameResponse, HttpStatus.OK);
+		return responseEntityGetProductsByNameResponse;
+	}
 	
 }
