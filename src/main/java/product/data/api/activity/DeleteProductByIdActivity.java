a237569fdd0e1d39361a37dc81fb81396b24fb42
Matthew Stibbins <matthewstibbins@gmail.com>
2022-06-27 08:58:47 -0700

    implement DeleteProductByIdActivity

--- src/main/java/product/data/api/activity/DeleteProductByIdActivity.java
+++ src/main/java/product/data/api/activity/DeleteProductByIdActivity.java
@@ -2,11 +2,15 @@ package product.data.api.activity;
 
 import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import product.data.api.manager.ProductManager;
+import product.data.api.model.Product;
+import product.data.api.response.DeleteProductByIdResponse;
 
 @RestController
 public class DeleteProductByIdActivity {
@@ -19,8 +23,16 @@ public class DeleteProductByIdActivity {
 	}
 	
 	@GetMapping("/deleteProductByIdUrlParams")
-	public ResponseEntity<DeleteProductByIdResponse> deleteProductByIdUrlParams() {
-		return null;
+	public ResponseEntity<DeleteProductByIdResponse> deleteProductByIdUrlParams(
+			@RequestParam Long productId
+	) {
+		Validate.notNull(productId, "productId cannot be null");
+		Product product = this.productManager.deleteProductById(productId);
+		DeleteProductByIdResponse deleteProductByIdResponse = DeleteProductByIdResponse
+				.builder().deletedProduct(product).build();
+		ResponseEntity<DeleteProductByIdResponse> responseEntityDeleteProductByIdResponse = 
+				new ResponseEntity<>(deleteProductByIdResponse, HttpStatus.OK);
+		return responseEntityDeleteProductByIdResponse;
 	}
 	
 }
--- src/main/java/product/data/api/response/DeleteProductByIdResponse.java
+++ src/main/java/product/data/api/response/DeleteProductByIdResponse.java
@@ -1,5 +1,18 @@
 package product.data.api.response;
 
-public class DeleteProductByIdResponse {
+import lombok.Data;
+import lombok.Builder;
 
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import product.data.api.model.Product;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeleteProductByIdResponse implements Serializable {
+	private Product deletedProduct;
 }
