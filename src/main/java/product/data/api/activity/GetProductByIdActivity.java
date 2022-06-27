ec36027538fa10668757da0f2a9638a4a03ad45f
Matthew Stibbins <matthewstibbins@gmail.com>
2022-06-27 04:47:47 -0700

    implement GetProductByIdActivity

--- src/main/java/product/data/api/activity/GetProductByIdActivity.java
+++ src/main/java/product/data/api/activity/GetProductByIdActivity.java
@@ -3,11 +3,13 @@ package product.data.api.activity;
 import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import product.data.api.manager.ProductManager;
+import product.data.api.model.Product;
 import product.data.api.response.GetProductByIdResponse;
 
 @RestController
@@ -21,7 +23,16 @@ public class GetProductByIdActivity {
 	}
 
 	@GetMapping("/getProductByIdUrlParams")
-	public ResponseEntity<GetProductByIdResponse> getProductById() {
-		
+	public ResponseEntity<GetProductByIdResponse> getProductByIdUrlParams(
+			@RequestParam Long productId
+	) {
+		Validate.notNull(productId, "productId cannot be null");
+		Product product = this.productManager.getProductById(productId);
+		GetProductByIdResponse getProductByIdResponse = GetProductByIdResponse.builder()
+				.product(product)
+				.build();
+		ResponseEntity<GetProductByIdResponse> responseEntityGetProductByIdResponse = 
+				new ResponseEntity<>(getProductByIdResponse, HttpStatus.OK);
+		return responseEntityGetProductByIdResponse;
 	}
 }
