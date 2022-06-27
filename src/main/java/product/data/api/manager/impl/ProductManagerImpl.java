029b315ca0afe6704dec72ed25dabea847042922
Matthew Stibbins <matthewstibbins@gmail.com>
2022-06-27 04:29:13 -0700

    implement ProductManager

--- src/main/java/product/data/api/manager/impl/ProductManagerImpl.java
+++ src/main/java/product/data/api/manager/impl/ProductManagerImpl.java
@@ -3,6 +3,7 @@ package product.data.api.manager.impl;
 import java.util.List;
 
 import org.apache.commons.lang3.Validate;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import product.data.api.dao.ProductDAO;
 import product.data.api.manager.ProductManager;
@@ -19,26 +20,31 @@ public class ProductManagerImpl implements ProductManager {
 	
 	@Override
 	public Product createProduct(String productName, String productDescription) {
-		// TODO Auto-generated method stub
-		return null;
+		Validate.notNull(productName, "productName cannot be null");
+		Validate.notNull(productDescription, "productDescription cannot be null");
+		Product product = this.productDAO.createProduct(productName, productDescription);
+		return product;
 	}
 
 	@Override
 	public List<Product> getProductsByName(String productName) {
-		// TODO Auto-generated method stub
-		return null;
+		Validate.notNull(productName, "productName cannot be null");
+		List<Product> productsList = this.productDAO.getProductsByName(productName);
+		return productsList;
 	}
 
 	@Override
 	public Product getProductById(Long productId) {
-		// TODO Auto-generated method stub
-		return null;
+		Validate.notNull(productId, "productId cannot be null");
+		Product product = this.productDAO.getProductById(productId);
+		return product;
 	}
 
 	@Override
 	public Product deleteProductById(Long productId) {
-		// TODO Auto-generated method stub
-		return null;
+		Validate.notNull(productId, "productId cannot be null");
+		Product product = this.productDAO.deleteProductById(productId);
+		return product;
 	}
 
 }
